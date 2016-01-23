30b31f4d7132dbfe1c3cc4a22263fa39ff8b8880
Patrick Cremin <pwcremin@gmail.com>
2016-01-23 15:46:08 -0600

    trying to push

---
+++ pages/categoryPreferencesSelection.js
@@ -0,0 +1,63 @@
+
+import React, {
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight
+} from 'react-native';
+
+var Selection = require('./selection/selection');
+var Display = require('./display/display');
+var Purchase = require('./purchase/purchase');
+
+var CategoryPreferencesSelection = React.createClass(
+    {
+        onSelection()
+        {
+            this.props.navigator.push({
+                component: Selection
+            } );
+        },
+
+        onDisplay()
+        {
+            this.props.navigator.push({
+                component: Display
+            } );
+        },
+
+        onPurchase()
+        {
+            this.props.navigator.push({
+                component: Purchase
+            } );
+        },
+
+        render()
+        {
+            return(
+                <View>
+                    <TouchableHighlight
+                        onPress={this.onSelection}
+                    >
+                        <Text>Selection</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        onPress={this.onDisplay}
+                    >
+                        <Text>Display</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        onPress={this.onPurchase}
+                    >
+                        <Text>Purchse</Text>
+                    </TouchableHighlight>
+                </View>
+
+            )
+        }
+    })
+
+module.exports = CategoryPreferencesSelection;
