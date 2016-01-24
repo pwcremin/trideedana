92ef01c510cb7e1a74ebd65b64573c25dc0a7b68
odrodrig <odrodrig>
2016-01-23 20:01:30 -0600

    Adding results.js

--- index.android.js
+++ index.android.js
@@ -4,48 +4,56 @@
  */
 'use strict';
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    Navigator,
+    TouchableHighlight,
+  //  StatusBarIOS
 } from 'react-native';
 
-class trideedana extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var Landing = require( './pages/landing' );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('trideedana', () => trideedana);
+//StatusBarIOS.setHidden(true);
+//StatusBarIOS.setNetworkActivityIndicatorVisible(false);
+
+var Trideedana = React.createClass( {
+
+    renderScene: function ( route, navigator )
+    {
+        return <route.component route={route} navigator={navigator}/>;
+    },
+
+    render() {
+        return (
+            <Navigator
+                initialRoute={{component: Landing}}
+                renderScene={this.renderScene}
+            />
+        )
+    }
+} );
+
+const styles = StyleSheet.create( {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+} );
+
+AppRegistry.registerComponent( 'trideedana', () => Trideedana );
